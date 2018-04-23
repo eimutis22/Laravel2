3b6a99b751668ac0c13738b9f23c9921a816487a
Eimutisg <eimutis.genys@gmail.com>
2018-04-23 08:21:23 +0100

    env

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,17 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel App
+                    Laravel
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laracasts.com">Login</a>
+                    <a href="https://laracasts.com">Register</a>
+                    <!-- <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a> -->
                 </div>
             </div>
         </div>
