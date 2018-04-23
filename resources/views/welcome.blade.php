d7cb8a19985daaf0f4932d2d00ad0e0795fc60b3
Eimutisg <eimutis.genys@gmail.com>
2018-04-23 15:14:04 +0100

    Set up for login/registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,8 +83,6 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laracasts.com">Login</a>
-                    <a href="https://laracasts.com">Register</a>
                     <!-- <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
